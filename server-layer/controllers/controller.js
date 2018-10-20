9cb993b2c3b2a3909b87967026f7ed465bcda356
simona.domnisoru <simona.domnisoru@gmail.com>
2018-10-20 15:49:22 +0200

    Use http to get the images

--- server-layer/controllers/controller.js
+++ server-layer/controllers/controller.js
@@ -1,17 +1,30 @@
-const Flickr = require('flickrapi');
-const { flickrOptions } = require('../utils/flickr_options');
+const https = require("https");
+const {URL, URLSearchParams} = require("url");
+const API_URL = "https://api.flickr.com/services/rest/";
+const {flickrOptions} = require('../utils/flickr_options');
 const parserImages = require('../utils/parser_images');
 
+const CALL_URL = new URL(API_URL);
+CALL_URL.search = new URLSearchParams(flickrOptions);
+
 const getImages = (req, res) => {
-  Flickr.tokenOnly(flickrOptions, (error, flickr) => {
-    flickr.photos.getRecent({
-      page: 1,
-      per_page: 10,
-    }, (err, result) => {
-      const images = parserImages(result.photos.photo);
-      res.json({ success: true, images });
-    });
-  });
+	https
+		.get(CALL_URL.href, function (response) {
+			// Continuously update stream with data
+			let data = "";
+			response.on("data", function (d) {
+				data += d;
+			});
+			response.on("end", function () {
+				// Data reception is done, do whatever with it!
+        const parsed = JSON.parse(data);
+        const images = parserImages(parsed.photos.photo);
+        res.json({ success: true, images });
+			});
+		})
+		.on("error", e => {
+			console.error(`Got error: ${e.message}`);
+		});
 };
 
 module.exports = getImages;
--- server-layer/utils/flickr_options.js
+++ server-layer/utils/flickr_options.js
@@ -1,7 +1,11 @@
 const flickrOptions = {
-  api_key: '125189ddfa7a0e66bfe3d9339832ba23',
-  secret: '0f63aadc6502450a',
-  user_id: '159971166@N03',
+	method: "flickr.photos.getRecent",
+	api_key: "125189ddfa7a0e66bfe3d9339832ba23",
+	format: "json",
+	nojsoncallback: 1,
+	per_page: 20,
+	page: 1,
+	extras: ["owner_name", "date_upload"]
 };
 
-module.exports = { flickrOptions };
+module.exports = {flickrOptions};
