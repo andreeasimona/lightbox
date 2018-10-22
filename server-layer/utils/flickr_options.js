3618d378a27bd37da18b165ccc5e9a56dd3ac281
Simona Domnisoru <simona.domnisoru@gmail.com>
2018-10-22 09:47:59 +0200

    Add demo

--- server-layer/utils/flickr_options.js
+++ server-layer/utils/flickr_options.js
@@ -3,7 +3,7 @@ const flickrOptions = {
   api_key: '125189ddfa7a0e66bfe3d9339832ba23',
   format: 'json',
   nojsoncallback: 1,
-  per_page: 50,
+  per_page: 100,
   extras: ['owner_name', 'date_upload', 'description'],
 };
 
--- src/api/images.js
+++ src/api/images.js
@@ -1,5 +1,8 @@
-const urlUsers = 'http://192.168.0.161:8142/api/images';
+const urlUsers = 'http://localhost:8142/api/images';
 
+/**
+ * Get the images for the backend api
+ */
 const getImages = (cb, page) => (
   fetch(`${urlUsers}/${page}`)
     .then(res => (res.json()))
